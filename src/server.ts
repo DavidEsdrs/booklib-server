6e0d04c11a076ad1e12b7fef549db2edc259b0a3
DavidEsdrs <carpina1983@gmail.com>
2022-03-13 15:20:14 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import "dotenv";
 import "../data";
 import express from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(router);
 
