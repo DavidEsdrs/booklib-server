ef99b67717093fdf28c0d16149ff508a37e03881
DavidEsdrs <carpina1983@gmail.com>
2022-03-13 21:39:55 -0300

    book now come from req.file

--- src/useCases/books/create/CreateBookController.ts
+++ src/useCases/books/create/CreateBookController.ts
@@ -10,7 +10,10 @@ export class CreateBookController {
     constructor(private service: ICreateBookService) {}
 
     async handle(req: Request, res: Response) {
-        const content = req.body;
+        const content = {
+            ...req.body,
+            content: req.file
+        };
         const book = await this.service.execute(content);
         return res.json(book);
     }
