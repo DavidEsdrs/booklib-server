c91a3e32f119f401f8ea96d4b0cbda9f361c7fa5
DavidEsdrs <carpina1983@gmail.com>
2022-03-14 22:22:31 -0300

    Add favorited_by many to many column to books table and its foreign key

---
+++ src/migrations/1647306715711-add_favorited_by_column_in_books_table.ts
@@ -0,0 +1,27 @@
+import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
+
+export class addFavoritedByColumnInBooksTable1647306715711 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.addColumn("books", new TableColumn({
+            name: "favorited_by",
+            type: "varchar",
+            isArray: true,
+            isNullable: true
+        }));
+
+        await queryRunner.createForeignKey("books", new TableForeignKey({
+            name: "books_users_fk",
+            referencedTableName: "users",
+            referencedColumnNames: [ "id" ],
+            columnNames: [ "favorited_by" ],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE"
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
