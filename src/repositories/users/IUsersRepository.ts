71541014a4c87720e9e13d6c5226e87b45ea3e1d
DavidEsdrs <carpina1983@gmail.com>
2022-03-14 23:15:47 -0300

    Add generic users repository implementation

---
+++ src/repositories/users/IUsersRepository.ts
@@ -0,0 +1,7 @@
+import { User } from "../../entities/User";
+
+export interface IUsersRepository {
+    usersAlreadyExists(email: string): Promise<boolean>;
+    create(args: Partial<User>): User;
+    save(args: User): Promise<void>;
+}
