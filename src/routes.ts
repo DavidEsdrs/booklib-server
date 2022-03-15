960bc58c79eb251ea724e237b9aef7e3151fd38c
DavidEsdrs <carpina1983@gmail.com>
2022-03-14 23:32:34 -0300

    Add user schema validator to the create user route

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,7 @@ import { buildReviewBook } from "./useCases/books/review/buildReviewBook";
 import { uploadBook } from "./utils/UploadBookParser";
 import { buildCreateUser } from "./useCases/users/create/buildCreateUser";
 import { validateBook } from "./middlewares";
+import { validateUser } from "./middlewares/validateUser";
 
 const router = Router();
 
@@ -17,6 +18,6 @@ router.get("/books/search/:id", (req, res) => buildGetBook().handle(req, res));
 router.delete("/books/:id", (req, res) => buildDeleteBook().handle(req, res));
 router.put("/books/:id", (req, res) => buildReviewBook().handle(req, res));
 
-router.post("/users", (req, res) => buildCreateUser().handle(req, res));
+router.post("/users", validateUser, (req, res) => buildCreateUser().handle(req, res));
 
 export { router };
