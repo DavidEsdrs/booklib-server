c434d373c5ad1139c7a66f75717a5fd61715e158
DavidEsdrs <carpina1983@gmail.com>
2022-03-14 23:14:30 -0300

    Add create user route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,12 @@
+import multer from "multer";
 import { Request, Router } from "express";
 import buildCreateBook from "./useCases/books/create/buildCreateBook";
 import { buildDeleteBook } from "./useCases/books/delete/buildDeleteBook";
 import { buildGetBooks } from "./useCases/books/getMany/buildGetBooks";
 import { buildGetBook } from "./useCases/books/getOne/buildGetBook";
 import { buildReviewBook } from "./useCases/books/review/buildReviewBook";
-import multer from "multer";
 import { uploadBook } from "./utils/UploadBookParser";
+import { buildCreateUser } from "./useCases/users/create/buildCreateUser";
 
 const router = Router();
 
@@ -14,5 +15,7 @@ router.get("/books", (req, res) => buildGetBooks().handle(req, res));
 router.get("/books/search/:id", (req, res) => buildGetBook().handle(req, res));
 router.delete("/books/:id", (req, res) => buildDeleteBook().handle(req, res));
 router.put("/books/:id", (req, res) => buildReviewBook().handle(req, res));
+
+router.post("/users", (req, res) => buildCreateUser().handle(req, res));
 
 export { router };
