60815a627e3ec67ede35c1254ce4db6ff612cafc
DavidEsdrs <carpina1983@gmail.com>
2022-03-15 21:52:38 -0300

    Add middleware to verify if the user is authenticated

---
+++ src/middlewares/ensureAuthenticatedUser.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+    sub: string;
+}
+
+export const ensureAuthenticatedUser = (req: Request, res: Response, next: NextFunction) => {
+    const auth = req.headers.authorization;
+
+    if(!auth) {
+        return res.status(401).json({ error: "Invalid Request!" });
+    }
+
+    const [ , token ] = auth.split(" ");
+
+    try {
+        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+        req.user_id = sub;
+        next();
+    }
+
+    catch {
+        return res.status(401).end();
+    }
+}
