033cc542c7ea8e382cbbb636684aa9f5a98e9b25
DavidEsdrs <carpina1983@gmail.com>
2022-03-28 09:23:35 -0300

    Remove old review book route

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,6 @@ import buildCreateBook from "./useCases/books/create/buildCreateBook";
 import { buildDeleteBook } from "./useCases/books/delete/buildDeleteBook";
 import { buildGetBooks } from "./useCases/books/getMany/buildGetBooks";
 import { buildGetBook } from "./useCases/books/getOne/buildGetBook";
-import { buildReviewBook } from "./useCases/books/review/buildReviewBook";
 import { uploadBook } from "./utils/UploadBookParser";
 import { buildCreateUser } from "./useCases/users/create/buildCreateUser";
 import { validateBook } from "./middlewares";
@@ -20,7 +19,6 @@ router.post("/books", ensureAuthenticatedUser, validateBook, multer(uploadBook.g
 router.get("/books", (req, res) => buildGetBooks().handle(req, res));
 router.get("/books/search/:id", (req, res) => buildGetBook().handle(req, res));
 router.delete("/books/:id",  (req, res) => buildDeleteBook().handle(req, res));
-router.put("/books/:id", (req, res) => buildReviewBook().handle(req, res));
 
 // Users
 router.post("/users", validateUser, (req, res) => buildCreateUser().handle(req, res));
