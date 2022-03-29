e116aff2f7a9ccb1fbc2b0419ebe43d3d83bd5b6
DavidEsdrs <carpina1983@gmail.com>
2022-03-28 23:02:57 -0300

    Catches only ServerError instances

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,9 +1,12 @@
 import { NextFunction } from "express";
 import { Response } from "express";
 import { Request } from "express";
+import { ServerError } from "../errors/ServerError";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if(err) {
-        return res.json({ error: err.name, message: err.message, status: 400 });
+export const errorHandler = (err: ServerError, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof ServerError) {
+        return res.status(err.status).json({ error: err.name, message: err.message });
+    } else {
+        return res.status(500).json({ error: "Internal server error", status: 500 })
     }
 }
