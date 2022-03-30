4a5aa98b2dc4d5930b9868fc8681e5b164facc9b
DavidEsdrs <carpina1983@gmail.com>
2022-03-29 22:17:46 -0300

    Get users repository and pass it as argument to the service instatiation

--- src/useCases/books/create/buildCreateBook.ts
+++ src/useCases/books/create/buildCreateBook.ts
@@ -1,11 +1,13 @@
 import { getCustomRepository } from "typeorm";
 import { BooksRepository } from "../../../repositories/books/implementations/BooksRepository";
+import { UsersRepository } from "../../../repositories/users/implementations/UsersRepository";
 import { CreateBook } from "./CreateBook";
 import { CreateBookController } from "./CreateBookController";
 
 const buildCreateBook = () => {
     const booksRepo = getCustomRepository(BooksRepository);
-    const service = new CreateBook(booksRepo);
+    const usersRepo = getCustomRepository(UsersRepository);
+    const service = new CreateBook(booksRepo, usersRepo);
     const controller = new CreateBookController(service);
     return controller;
 }
