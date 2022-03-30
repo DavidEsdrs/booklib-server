284cdc091a555d035e577b0507d80741f91e5a92
DavidEsdrs <carpina1983@gmail.com>
2022-03-29 22:23:12 -0300

    Get user_id and the file_name from the request

--- src/useCases/books/create/CreateBookController.ts
+++ src/useCases/books/create/CreateBookController.ts
@@ -11,9 +11,11 @@ export class CreateBookController {
     constructor(private service: ICreateBookService) {}
 
     async handle(req: Request, res: Response) {
+        const { user_id } = req;
         const content = {
             ...req.body,
-            content: req.file_props.file_name + "." + req.file_props.file_type
+            content: req.file_props.file_name,
+            published_by: user_id
         };
         const book = await this.service.execute(content);
         return res.json(book);
