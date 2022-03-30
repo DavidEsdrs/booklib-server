72d2e6a5ecdb796550ba15338edd54b63ca46dfd
DavidEsdrs <carpina1983@gmail.com>
2022-03-29 22:12:11 -0300

    Check if the request is made by the user who published the user && return the deleted book

--- src/useCases/books/delete/DeleteBook.ts
+++ src/useCases/books/delete/DeleteBook.ts
@@ -1,13 +1,21 @@
+import { instanceToPlain } from "class-transformer";
+import { UnauthorizedRequestError } from "../../../errors/ServerError";
 import { IBooksRepository } from "../../../repositories/books/IBooksRepository";
 import { IDeleteBookDTO } from "./IDeleteBookDTO";
 
 export class DeleteBook {
     constructor(private booksRepo: IBooksRepository) {}
 
-    async execute({ id }: IDeleteBookDTO) {
-        await this.booksRepo.deleteBook(id);
+    async execute({ id, user }: IDeleteBookDTO) {
+        const book = await this.booksRepo.getBook({ id, relations: ["published_by"] });
+        if(book.published_by.id !== user) {
+            throw new UnauthorizedRequestError();
+        }
+        const file = book.content;
+        await this.booksRepo.deleteBook({ id, file });
         return {
-            response: `Book ${id} deleted`
+            response: `Book ${id} deleted`,
+            book: instanceToPlain(book)
         };
     }
 }
