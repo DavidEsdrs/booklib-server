69a38095c59bebfa1b391ae6519701f72eeb6fe0
DavidEsdrs <carpina1983@gmail.com>
2022-03-29 22:19:50 -0300

    Create book with published_by field filled

--- src/useCases/books/create/CreateBook.ts
+++ src/useCases/books/create/CreateBook.ts
@@ -1,15 +1,19 @@
+import { instanceToPlain } from "class-transformer";
+import { Book } from "../../../entities/Book";
 import { IBooksRepository } from "../../../repositories/books/IBooksRepository";
+import { IUsersRepository } from "../../../repositories/users/IUsersRepository";
 import { ICreateBookDTO } from "./ICreateBookDTO";
 
 export class CreateBook {
-    constructor(private booksRepo: IBooksRepository) {}
+    constructor(private booksRepo: IBooksRepository, private usersRepo: IUsersRepository) {}
 
     async execute(createBookDTO: ICreateBookDTO) {
+        const user = await this.usersRepo.findOne({ id: createBookDTO.published_by });
         const book = this.booksRepo.create({
             ...createBookDTO,
-            content: createBookDTO.content
+            published_by: user
         });
         await this.booksRepo.save(book);
-        return book;
+        return instanceToPlain(book) as Book;
     }
 }
